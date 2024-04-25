72057b223687a7e68feb4c8fb23f29bf1593c3e7
Beshoy Bahaa <bahaa_beshoy@yahoo.com>
2024-04-25 21:21:43 +0200

    video1

---
+++ zombieGame/Assets/script/mouseMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseMovement : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Locking the cursor to the middle of the screen and making it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Getting the mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Rotation around the x axis (Look up and down)
+        xRotation -= mouseY;
+
+        //Clamp the rotation
+        xRotation = Mathf.Clamp(xRotation,-90f,90f);
+
+        // Rotation around the x axis(Look left and right)
+        yRotation += mouseX;
+
+        //apply rotation to our transform
+        transform.localRotation = Quaternion.Euler(xRotation,yRotation,0f);
+
+    }
+}
