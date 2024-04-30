988f569c0bcc76c1562199559729cb7399f57818
Beshoy Bahaa <bahaa_beshoy@yahoo.com>
2024-04-30 05:39:56 +0300

    video 17
    zombie die animation🎉🎉

--- zombieGame/Assets/script/zombie.cs
+++ zombieGame/Assets/script/zombie.cs
@@ -1,28 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class zombie : MonoBehaviour
 {
 
     [SerializeField] private int HP = 100;
     private Animator animator;
+    private NavMeshAgent navAgent;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
     public void TakeDamage(int damageAmount){
         HP -= damageAmount;
 
         if(HP <= 0){
-            // animator.SetTrigger("DIE");
-            Destroy(gameObject);
+            animator.SetTrigger("DIE1");
+            // Destroy(gameObject);
         }else{
             //animator.SetTrigger("DAMAGE");
         }
         print("HP : "+HP);
     }
-    
+
+    private void Update() {
+        if (navAgent.velocity.magnitude > 0.1f){
+            animator.SetBool("isWalking",true);
+        }else{
+            animator.SetBool("isWalking", false);
+        }
+    }
 }
